496981319997450fe1361ece5974d2acd7714d29
thejgdt <theaberchio@gmail.com>
2023-10-22 21:33:32 +0700

    implement javascript alert() function

---
+++ js/random.js
@@ -0,0 +1,18 @@
+function loadRandomQuote() {
+  var filePath = "assets/random-quotes.txt";
+
+  fetch(filePath)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      var quotesArray = data.split("\n");
+      var randomQuote =
+        quotesArray[Math.floor(Math.random() * quotesArray.length)];
+      alert(randomQuote);
+    })
+    .catch((error) => console.error("Error fetching random quotes:", error));
+}
